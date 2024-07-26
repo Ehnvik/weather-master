36e17755f48f25128bfb3804bbdf7324113b76b5
ehnvik <gustav.ehnvik@gmail.com>
2024-07-26 17:39:27 +0200

    deleted dist and vercel file

--- server/dist/routes/location/locationRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const locationControllers_1 = require("../../controller/locationControllers");
-const locationRouter = (0, express_1.Router)();
-locationRouter.get("/name", locationControllers_1.getLocationsByName);
-locationRouter.get("/coordinates", locationControllers_1.getLocationByCoordinates);
-exports.default = locationRouter;
--- server/dist/routes/weather/weatherRoutes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const weatherControllers_1 = require("../../controller/weatherControllers");
-const weatherRouter = (0, express_1.Router)();
-weatherRouter.get("/", weatherControllers_1.getWeatherData);
-exports.default = weatherRouter;
