eb317598ea57cf3143e466861efb46cecd9f54ab
ehnvik <gustav.ehnvik@gmail.com>
2024-07-26 18:25:05 +0200

    restored server.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
-import config from "./config/dotenvConfig";
 
 const app = express();
 
@@ -19,17 +18,5 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
-
-const run = () => {
-  try {
-    app.listen(config.PORT, () => {
-      console.log(`Server is listening on http://localhost:${config.PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-run();
 
 export default app;
