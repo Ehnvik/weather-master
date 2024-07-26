51cc4a57374ff0271f6478a4728f054b0905cc51
ehnvik <gustav.ehnvik@gmail.com>
2024-07-26 18:19:57 +0200

    moved server logic to app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { router } from "./routes";
+import config from "./config/dotenvConfig";
 
 const app = express();
 
@@ -18,5 +19,17 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
+
+const run = () => {
+  try {
+    app.listen(config.PORT, () => {
+      console.log(`Server is listening on http://localhost:${config.PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+run();
 
 export default app;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,14 @@
-import app from "./app";
-import config from "./config/dotenvConfig";
+// import app from "./app";
+// import config from "./config/dotenvConfig";
 
-const run = () => {
-  try {
-    app.listen(config.PORT, () => {
-      console.log(`Server is listening on http://localhost:${config.PORT}`);
-    });
-  } catch (error) {
-    console.error(error);
-  }
-};
+// const run = () => {
+//   try {
+//     app.listen(config.PORT, () => {
+//       console.log(`Server is listening on http://localhost:${config.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
 
-run();
+// run();
