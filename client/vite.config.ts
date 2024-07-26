aee4893fd8d30d7ce0c4e920aff2515e84f4506a
ehnvik <gustav.ehnvik@gmail.com>
2024-07-26 13:46:08 +0200

    updated vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[hash].[ext]",
+        entryFileNames: "assets/[name].[hash].js",
+        chunkFileNames: "assets/[name].[hash].js",
+      },
+    },
+  },
+});
