636c1c8214616d867211564b8fc5dc873dc5f7b3
ehnvik <gustav.ehnvik@gmail.com>
2024-08-15 16:54:11 +0200

    created weather details for desktop

--- client/src/components/WeatherDetails/WeatherDetails.tsx
+++ client/src/components/WeatherDetails/WeatherDetails.tsx
@@ -3,6 +3,7 @@ import uv from "../../assets/uv.png";
 import clouds from "../../assets/clouds-percentage.png";
 import feelsLike from "../../assets/temperature-feels-like.png";
 import windDegrees from "../../assets/wind.png";
+import visibility from "../../assets/visibility.png";
 import "./WeatherDetails.scss";
 import { WeatherLocationData } from "../../models/Weather/Classes/WeatherLocationData";
 import { useScreenWidth } from "../../hooks/useScreenWidth";
@@ -15,6 +16,11 @@ export const WeatherDetails = ({ weatherLocationData }: IWeatherInfoProps) => {
   const { screenWidth } = useScreenWidth();
   return (
     <div className="details">
+      {screenWidth >= 1000 && (
+        <h4 className="details__weather-summary">
+          {weatherLocationData.weatherData.daily[0].summary}
+        </h4>
+      )}
       <div className="details__items-container">
         <div className="details__item">
           <img className="details__icon" src={uv} alt="UV Icon" />
@@ -76,41 +82,16 @@ export const WeatherDetails = ({ weatherLocationData }: IWeatherInfoProps) => {
               </div>
             </div>
             <div className="details__item">
-              <img className="details__icon" src={wind} alt="Wind Speed Icon" />
+              <img
+                className="details__icon"
+                src={visibility}
+                alt="Visibility Icon"
+              />
               <div className="details__level">
                 <p className="details__unit">
-                  {weatherLocationData.formattedUnits.windSpeed} m/s
+                  {weatherLocationData.formattedUnits.visibility} km
                 </p>
-                <p className="details__text">Wind Speed</p>
-              </div>
-            </div>
-          </div>
-          <div className="details__items-container">
-            <div className="details__item">
-              <img className="details__icon" src={uv} alt="UV Icon" />
-              <div className="details__level">
-                <p className="details__unit">
-                  {weatherLocationData.weatherData.current.uvi}
-                </p>
-                <p className="details__text">UV Index</p>
-              </div>
-            </div>
-            <div className="details__item">
-              <img className="details__icon" src={clouds} alt="Clouds Icon" />
-              <div className="details__level details__level">
-                <p className="details__unit">
-                  {weatherLocationData.weatherData.current.clouds}%
-                </p>
-                <p className="details__text">Cloud Cover</p>
-              </div>
-            </div>
-            <div className="details__item">
-              <img className="details__icon" src={wind} alt="Wind Speed Icon" />
-              <div className="details__level">
-                <p className="details__unit">
-                  {weatherLocationData.formattedUnits.windSpeed} m/s
-                </p>
-                <p className="details__text">Wind Speed</p>
+                <p className="details__text">Visibility</p>
               </div>
             </div>
           </div>
